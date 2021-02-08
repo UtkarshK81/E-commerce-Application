181da199e7d9cacedd588b07b869df1b50a778b6
UtkarshK81 <utkarshk.web74@gmail.com>
2021-02-08 14:39:01 +0530

    Towards DevOps

--- OnlineShopping/src/main/java/com/cg/zmart/entity/Product.java
+++ OnlineShopping/src/main/java/com/cg/zmart/entity/Product.java
@@ -1,15 +1,8 @@
 package com.cg.zmart.entity;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -18,34 +11,31 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "PRODUCTS")
 public class Product {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
-	
+
 	@NotNull
-	@Size(min = 1,max = 255,message = "Give your product a name.")
+	@Size(min = 1, max = 255, message = "Give your product a name.")
 	private String productName;
-	
+
 	@NotNull
 	@Min(1)
 	private double unitPrice;
-	
+
 	@NotNull
 	private long productQuantity;
-	
+
 	@NotNull
 	private String productDescription;
-	
+
 	@NotNull
 	private String productCategory;
-	
-	@OneToMany(targetEntity = Review.class,cascade = CascadeType.ALL)
-	@JoinColumn(name = "pr_fid",referencedColumnName = "id")
-	private List<Review> reviews=new ArrayList<Review>();
-	
+
 	@NotNull
 	private String imageUrl;
+
 	public Product() {
 	}
 
@@ -105,11 +95,4 @@ public class Product {
 		this.imageUrl = imageUrl;
 	}
 
-	public List<Review> getReviews() {
-		return reviews;
-	}
-
-	public void setReviews(List<Review> reviews) {
-		this.reviews = reviews;
-	}
 }
